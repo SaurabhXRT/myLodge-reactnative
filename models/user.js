740f6f41198292cbfac140b439f2b77eefdd728f
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-01-15 17:20:30 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,6 +5,12 @@ const userSchema = new mongoose.Schema({
   name: String,
   mobile: { type: String, unique: true },
   password: String,
+  age: Number,
+  gender: String,
+  address: String,
+  studyingIn: String,
+  bio: String,
+  profileImage: String, // Added field for Cloudinary link
 });
 
 const User = mongoose.model('User', userSchema);
