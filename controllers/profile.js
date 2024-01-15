ac5379cece0092bc4ede418f97bef532851348f2
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-01-15 19:40:23 +0530

    Update profile.js

--- controllers/profile.js
+++ controllers/profile.js
@@ -40,40 +40,24 @@ router.get('/profiledata', async (req, res) => {
 
 router.put('/updateprofile', async (req, res) => {
   try {
-    // Get the user ID from the authenticated user's token
     const userId = req.userId;
-
-    // Fetch the user from the database
     const user = await User.findById(userId);
-
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
-
-    // Update user profile based on request body
     user.name = req.body.name || user.name;
     user.age = req.body.age || user.age;
     user.gender = req.body.gender || user.gender;
     user.address = req.body.address || user.address;
     user.studyingIn = req.body.studyingIn || user.studyingIn;
     user.bio = req.body.bio || user.bio;
-
-    // If a new profileImage is uploaded, update the Cloudinary link
     if (req.file) {
-      // Upload the new profileImage to Cloudinary
       const result = await cloudinary.uploader.upload(req.file.path);
-
-      // Update the user's profileImage with the Cloudinary link
+      console.log(result");
       user.profileImage = result.secure_url;
-
-      // Remove the temporary file after uploading to Cloudinary
       fs.unlinkSync(req.file.path);
     }
-
-    // Save the updated user profile
     await user.save();
-
-    // Return the updated user profile
     res.json({
       name: user.name,
       mobile: user.mobile,
