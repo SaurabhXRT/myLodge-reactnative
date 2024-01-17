897d56535c862ec1a76491803e0ba3a0a8037abe
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-01-17 19:41:11 +0530

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -170,5 +170,22 @@ router.get("/fetch-dues/:roomId", async (req, res) => {
   }
 });
 
+router.get('/fetch-payments/:roomId', async (req, res) => {
+  try {
+    const roomId = req.params.roomId;
+    const room = await Room.findById(roomId);
+
+    if (!room) {
+      return res.status(404).json({ error: 'Room not found' });
+    }
+
+    const payments = room.payment;
+    res.json(payments);
+  } catch (error) {
+    console.error('Error fetching payments:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 
 module.exports = router;
