757bb90221945c7772dabdf80e53e950530b5c0e
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-01-18 19:30:01 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -69,7 +69,12 @@ router.get('/fetch-payments', async (req, res) => {
 router.post('/posts',uploads.single('image'), async (req, res) => {
   try {
     const text = req.body;
-    const createdBy = req.user._id;
+    const userId = req.userId;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    const createdBy = userId;
 
     const post = new Post({ text, image, createdBy });
     await post.save();
