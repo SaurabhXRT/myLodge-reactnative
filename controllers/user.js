01c2b2a3182ff4526eea56ae7a4b8e9be1d7de21
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-01-19 23:51:32 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -75,14 +75,15 @@ router.post('/posts',uploads.single('image'), async (req, res) => {
       return res.status(404).json({ error: 'User not found' });
     }
     const createdBy = userId;
-    if(req.file){
-    const result = await cloudinary.uploader.upload(req.file.path);
-    console.log("result");
-    };
+    let image;
+    if (req.file) {
+      const result = await cloudinary.uploader.upload(req.file.path);
+      image = result.secure_url;
+    }
 
     const post = new Post({
       text,
-      image: result.secure_url,
+      image,
       createdBy 
     });
     await post.save();
